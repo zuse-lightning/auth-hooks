0fb4a297d7e7cd4f8b93adb6a0d4aba7c102adef
James Geib <james@zuse.com>
2024-03-15 14:23:19 -0400

    added missing code

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./containers/App";
+import { BrowserRouter as Router } from "react-router-dom";
+import { AuthProvider } from "./provider/AuthProvider";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  
-    <App />
-  
+  <Router>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </Router>
 );
 
 reportWebVitals();
